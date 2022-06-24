6e3f8e47685d62eff65c966c4269717e674755c9
Serov Ivan <ISerov@croc.ru>
2022-06-24 21:43:18 +0300

    Добавлены функции LoginPage, которые проверяют наличие форм авторизации и регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 @pytest.mark.test_guest_can_go_to_login_page
 def test_guest_can_go_to_login_page(browser):
@@ -21,4 +21,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+@pytest.mark.test_guest_on_the_right_page_and_can_use_registration
+def test_guest_on_the_right_page_and_can_use_registration(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_register_form()
+
+@pytest.mark.test_guest_on_the_right_page_and_can_use_authorization
+def test_guest_on_the_right_page_and_can_use_authorization(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+
 
