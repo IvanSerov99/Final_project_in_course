5245de0bd82c146ba63abb5eb3c9a0380b56a0ca
Serov Ivan <ISerov@croc.ru>
2022-06-25 17:02:28 +0300

    Исправлена маркер у функции test_that_the_guest_can_add_an_product_to_the_basket

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ import random
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 '''
 
-@pytest.mark.for_test
+@pytest.mark.need_review
 def test_that_the_guest_can_add_an_product_to_the_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
